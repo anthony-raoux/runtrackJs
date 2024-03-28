b44c6dd44dec7a61253dc11f333b3770223ddad7
anthony-raoux <anthony.raoux@laplateforme.io>
2024-03-28 16:07:31 +0100

    correction jour 02

--- jour04/job02/script.js
+++ jour04/job02/script.js
@@ -1,16 +1,12 @@
 function jsonValueKey(jsonString, key) {
     const jsonObject = JSON.parse(jsonString);
-
     if (key in jsonObject) {
-
         return jsonObject[key];
     } else {
-        
         return "Clé non trouvée";
     }
 }
 
-   const jsonString = '{"city": "Marseille"}';
-   const key = 'city';
-   const value = jsonValueKey(jsonString, key);
-   document.getElementById('result').textContent = `La valeur associée à la clé "${key}" est "${value}".`;
+const jsonString = '{"name": "LaPlateforme_", "address": "8 rue d\'hozier", "city": "Marseille", "nb_staff": "11", "creation": "2019"}';
+const value = jsonValueKey(jsonString, 'city');
+console.log(value);
